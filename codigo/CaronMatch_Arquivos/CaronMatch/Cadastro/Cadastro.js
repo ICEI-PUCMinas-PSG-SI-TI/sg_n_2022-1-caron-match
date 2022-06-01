d2d09d406f421d3e1cd6bff949d5b693d5a11c30
Isadora Horta Rates <104460444+isadorarates@users.noreply.github.com>
2022-06-01 14:34:05 -0300

    Update Cadastro.js
    Coloquei o armazenamento local; As infos aparecem no console. Quando clica cadastrar vai para a pagina de login.

--- codigo/CaronMatch_Arquivos/CaronMatch/Cadastro/Cadastro.js
+++ codigo/CaronMatch_Arquivos/CaronMatch/Cadastro/Cadastro.js
@@ -1,3 +1,16 @@
+window.onload = () => {
+
+    email.onchange = () => console.log(email.value);
+    nome.onchange = () => console.log(nome.value);
+    sobrenome.onchange = () => console.log(sobrenome.value);
+    password.onchange = () => console.log(password.value);
+    passconfirmation.onchange = () => console.log(passconfirmation.value);
+    telefone.onchange = () => console.log(telefone.value);
+    dar.onchange = () => console.log(dar.value);
+    pegar.onchange = () => console.log(pegar.value);
+    lembre.onchange = () => console.log(lembre.value);
+
+
 cadastro.onsubmit = (event) => {
     localStorage.setItem('email', email.value);
     localStorage.setItem('nome', nome.value);
@@ -5,13 +18,16 @@ cadastro.onsubmit = (event) => {
     localStorage.setItem('password', password.value);
     localStorage.setItem('passconfirmation', passconfirmation.value);
     localStorage.setItem('telefone', telefone.value);
-    if (dar.checked){
+    if (dar.checked) {
         localStorage.setItem('escolha', dar.value);
     }
     else {
         localStorage.setItem('escolha', pegar.value);
     }
- 
+
 
     event.preventDefault();
-}
+    window.location.href = "/sg_n_2022-1-caron-match-master/codigo/CaronMatch_Arquivos/CaronMatch/Login/Login.html";
+} }
+
+
