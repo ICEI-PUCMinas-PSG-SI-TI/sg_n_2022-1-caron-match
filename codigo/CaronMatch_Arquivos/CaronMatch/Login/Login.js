1eda4b784386c706ef11ce281adca04661ba64ba
Isadora Horta Rates <104460444+isadorarates@users.noreply.github.com>
2022-06-07 21:59:51 -0300

    Add files via upload

--- codigo/CaronMatch_Arquivos/CaronMatch/Login/Login.js
+++ codigo/CaronMatch_Arquivos/CaronMatch/Login/Login.js
@@ -1,11 +1,19 @@
 window.onload = () => {
-    email.onchange = () => console.log(email.value);
-    senha.onchange = () => console.log(senha.value);
-    lembre.onchange = () => console.log(lembre.value);
-
-    localStorage.setItem('email', email.value);
-    localStorage.setItem('senha', senha.value);
-    localStorage.setItem('lembre', lembre.value);
-    
- }
-    
+login.onsubmit = (event) => {
+    var emaildigitado = email.value;
+    var emailexistente = localStorage.getItem(emaildigitado + "-email");
+    if (emailexistente == emaildigitado) {
+        var senhadigitada = senha.value;
+        var senhaexistente = localStorage.getItem(emaildigitado + "-password")
+        if (senhaexistente == senhadigitada) {
+            event.preventDefault();
+            window.location.href = "/codigo/CaronMatch_Arquivos/CaronMatch/Profile/profile.html";
+        }
+        else {
+            window.alert("Senha incorreta. Tente novamente.")
+        }
+    }
+    else {
+        window.alert("E-mail não encontrado.")
+    }
+} }
