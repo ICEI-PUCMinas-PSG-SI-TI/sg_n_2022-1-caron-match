c64e72826eca9e672f68d5f66c05deb92cb1b3a5
Isadora Horta Rates <104460444+isadorarates@users.noreply.github.com>
2022-06-07 21:58:24 -0300

    Add files via upload

--- codigo/CaronMatch_Arquivos/CaronMatch/Cadastro/Cadastro.js
+++ codigo/CaronMatch_Arquivos/CaronMatch/Cadastro/Cadastro.js
@@ -4,30 +4,38 @@ window.onload = () => {
     nome.onchange = () => console.log(nome.value);
     sobrenome.onchange = () => console.log(sobrenome.value);
     password.onchange = () => console.log(password.value);
-    passconfirmation.onchange = () => console.log(passconfirmation.value);
     telefone.onchange = () => console.log(telefone.value);
     dar.onchange = () => console.log(dar.value);
     pegar.onchange = () => console.log(pegar.value);
-    lembre.onchange = () => console.log(lembre.value);
 
 
-cadastro.onsubmit = (event) => {
-    localStorage.setItem('email', email.value);
-    localStorage.setItem('nome', nome.value);
-    localStorage.setItem('sobrenome', sobrenome.value);
-    localStorage.setItem('password', password.value);
-    localStorage.setItem('passconfirmation', passconfirmation.value);
-    localStorage.setItem('telefone', telefone.value);
-    if (dar.checked) {
-        localStorage.setItem('escolha', dar.value);
+
+    cadastro.onsubmit = (event) => {
+        var emaildigitado = email.value;
+      
+        var emailexistente = localStorage.getItem(emaildigitado + "-email");
+        if (emailexistente == emaildigitado) {
+            window.alert("Email já cadastrado.")
+        }
+        else{
+            localStorage.setItem(emaildigitado + '-email', email.value);
+            localStorage.setItem(emaildigitado + '-nome', nome.value);
+            localStorage.setItem(emaildigitado + '-sobrenome', sobrenome.value);
+            localStorage.setItem(emaildigitado + '-password', password.value);
+            localStorage.setItem(emaildigitado + '-telefone', telefone.value);
+            if (dar.checked) {
+                localStorage.setItem(emaildigitado + '-escolha', dar.value);
+            }
+            else {
+                localStorage.setItem(emaildigitado + '-escolha', pegar.value);
+               
+            }
+            event.preventDefault();
+            window.location.href = "/codigo/CaronMatch_Arquivos/CaronMatch/Login/Login.html";
+        }
+       
+
     }
-    else {
-        localStorage.setItem('escolha', pegar.value);
-    }
-
-
-    event.preventDefault();
-    window.location.href = "/sg_n_2022-1-caron-match-master/codigo/CaronMatch_Arquivos/CaronMatch/Login/Login.html";
-} }
+}
 
 
