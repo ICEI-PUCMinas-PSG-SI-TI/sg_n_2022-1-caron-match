3759f6f2af6c568eb64626833b9f84cbca114663
SamuelrcvpPUCMG <104330363+SamuelrcvpPUCMG@users.noreply.github.com>
2022-07-08 20:46:12 -0300

    Update Paginafinal.js

--- codigo/TelaFinal/Paginafinal.js
+++ codigo/TelaFinal/Paginafinal.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     
     var nome = localStorage.getItem('usuariologado-nome');
-    document.getElementById('usuario').innerHTML = "Bem vindo " + nome;
+    document.getElementById('usuario').innerHTML = "Bem vindo " + nome +" !";
 }
 
