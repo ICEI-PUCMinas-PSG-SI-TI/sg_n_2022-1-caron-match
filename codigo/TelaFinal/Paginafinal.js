9e29645b777b7eee20ac773a1d4f4a9b98fcbffe
LucasEC-Aguiar <104530476+LucasEC-Aguiar@users.noreply.github.com>
2022-07-13 11:42:09 -0300

    Update Paginafinal.js

--- codigo/TelaFinal/Paginafinal.js
+++ codigo/TelaFinal/Paginafinal.js
@@ -11,3 +11,20 @@ function funcaoLogout(){
    
        
    }
+
+function initMap() { 
+   
+    const p1 = { lat: -19.858906543390017, lng: -43.91895769784901 };
+   
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: p1,
+    });
+   
+    const marker = new google.maps.Marker({
+      position: p1,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
